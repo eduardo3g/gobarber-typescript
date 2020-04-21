22bac52dedcd3aec134b5da8adb3510c19a54775
= <=>
2020-04-20 23:09:43 -0300

    config: Importing TypeScript files.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.use(express.json());
+app.use(routes);
 
-app.get('/', (request, response) => response.json({ message: 'Hello world!' }));
+app.use(express.json());
 
 app.listen(3333, () => {
   console.log('The server is running on port 3333.');
