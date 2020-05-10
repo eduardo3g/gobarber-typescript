84913f1df0705ec539d4107df059e685aa502afa
eduardo3g <eduardosbrasil10@gmail.com>
2020-05-09 23:28:37 -0300

    First unit test (Create appointment).

---
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,24 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('Should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '154334356753456',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('154334356753456');
+  });
+
+  // it('Should NOT be able to create two appointments on the same datetime', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+});
