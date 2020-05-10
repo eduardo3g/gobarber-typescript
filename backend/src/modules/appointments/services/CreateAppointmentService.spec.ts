4e8dd5d5b5d6fb5e9dcb2d5fc433399196adb60c
eduardo3g <eduardosbrasil10@gmail.com>
2020-05-10 19:41:07 -0300

    Test to reject appointments scheduled on the same date.

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -18,7 +19,25 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('154334356753456');
   });
 
-  // it('Should NOT be able to create two appointments on the same datetime', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('Should NOT be able to create two appointments on the same datetime', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '154334356753456',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '154334356753456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
